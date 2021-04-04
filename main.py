6feecc84eb354beeeb6bb35629be1c148c7c6f66
sharon <sharonvazana@gmail.com>
2021-04-04 22:09:22 +0300

    comments

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-""" team members: Hadar Amsalem, Sharon Vazana"""
+""" assignment 2
+    team members: Hadar Amsalem, Sharon Vazana
+    git link: https://github.com/sharon-v/numeric2.git"""
+# gaussian elimination--------------
+# non singular if inverse exists
+# inverse exists if (det != 0)
+# LU--------------------------------
+# singular if no inverse -> (det == 0)
+# solve with (inverse U) * (inverse L) * (vector b)
+
+
+# ############## det ##############
