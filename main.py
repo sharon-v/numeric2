26e0fb41240ef6e88b99c50aba305c1cf2ebde2b
sharon <sharonvazana@gmail.com>
2021-04-05 22:14:20 +0300

    findU func

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # gaussian elimination--------------
 # non singular if inverse exists
 # inverse exists if (det != 0)
+# solve with (inverse A) * (vector b)
 # LU--------------------------------
 # singular if no inverse -> (det == 0)
 # solve with (inverse U) * (inverse L) * (vector b)
@@ -13,29 +14,50 @@
 # (m*n) * (n*p) = m*p
 
 # ############# det ##############
-def det(A):
-    pass
 
 
-# ########## swap lines ##########
+# def det(a):
+#     if len(a) and len(a[0]) is 2:
+#         return a[0][0] * a[1][1] - a[0][1] * a[1][0]
+#     i = 0
+#     x = 1
+#     for j in range(len(a[0])):
+#         return a[i][j]
 
 
-# mult matrics
-# def multMatrics(a, b):
-#     # assume same size
-#     for row in a:
-#         for col in a:
-#
+# def minor(b, row, col):
+#     if row >= len(b) and col >= len(b):
+#         return b
+#     c = makeMatrics(len(b) - 1, len(b) - 1)
+#     x = 0
+#     y = 0
+#     for i in range(len(b)):
+#         for j in range(len(b[0])):
+#             if i is not row and j is not col:
+#                 c[x][y] = b[i][j]
+#                 if y is len(c[0]) - 1:
+#                     x += 1
+#                     y = 0
+#                 else:
+#                     y += 1
+#     return c
 
-def test():
-    a = [[3, 2], [8, 6]]    # try different sizes!!
-    b = [[3, 4], [2, 9]]
+
+def findU(a):
+    for i in range(len(a)):
+        if a[i][i] is 0:
+            while j < len(a):
+                if a[i][j] is not 0:
+
+
+
+
+def multMatrics(a, b):
     c = makeMatrics(len(a), len(b[0]))
     for row in range(len(a)):
         for col in range(len(b[0])):
             for x in range(len(a)):
                 c[row][col] += (a[row][x] * b[x][col])
-    print(c)
 
 
 def makeMatrics(row, col):
@@ -63,7 +85,11 @@ def swapRow(a, r1, r2):
     return a
 
 
-test()
-c = setMatrics()
-print(c)
-print(swapRow(c, 1, 0))
+def drive():
+    # print(minor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2, 2))
+    c = setMatrics()
+    print(c)
+    print(swapRow(c, 1, 0))
+
+
+drive()
